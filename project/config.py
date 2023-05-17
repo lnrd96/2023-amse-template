b71655314d84a9f1b12e913a34ed46851e38d1c9
--replace-all <leonard.fischer@fau.de>
2023-05-17 09:43:20 +0200

    Added 2nd data source to config.

--- project/config.py
+++ project/config.py
@@ -1,1 +1,4 @@
 DATA_SOURCE_ACCIDENTS = 'https://www.opengeodata.nrw.de/produkte/transport_verkehr/unfallatlas'
+DATA_SOURCE_ROAD_TYPES = 'https://nominatim.openstreetmap.org/reverse?lat={latidude}&'\
+                         'lon={longidude}&format=jsonv2&zoom=17'
+PROJECT_ROOT = '/Users/leonardfischer/Uni/sem_2/de/2023-amse-template/project'
