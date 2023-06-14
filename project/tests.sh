8ad94ef3b9b0375deed92f4987e34a7e850ddce8
--replace-all <leonard.fischer@fau.de>
2023-06-14 14:00:03 +0200

    Fixed bug in config file.

--- project/tests.sh
+++ project/tests.sh
@@ -1,3 +1,5 @@
+# INFO: Working directory should be <project/>
+
 # source ../../venv/bin/activate
 # python3 -m pip install -r requirements.txt
 python3 -m unittest unit_tests/test_DatabaseHandler.py
