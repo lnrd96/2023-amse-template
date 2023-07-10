6a314c98ca21d35050c6758537c9fda793abe9a1
--replace-all <leonard.fischer@fau.de>
2023-07-10 19:38:41 +0200

    Expanded regex.

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -16,11 +16,16 @@ def automated_data_pipeline():
     urllib.request.urlretrieve(DATA_URI, 'data.zip')
     with zipfile.ZipFile('data.zip', 'r') as zip_ref:
         zip_ref.extractall('data')
-    df = pd.read_csv('data/stops.txt', encoding='ISO-8859-1', delimiter=',')  # encoding keeps german umlauts
+    df = pd.read_csv('data/stops.txt', encoding='utf-8', delimiter=',')  # encoding keeps german umlauts
 
     # keep only specified columns
     columns_to_keep = ['stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'zone_id']
     df = df.drop(columns=set(df.columns) - set(columns_to_keep))
+    df['stop_id'] = df['stop_id'].astype(int)
+    df['stop_name'] = df['stop_name'].astype(str)
+    df['stop_lat'] = df['stop_lat'].astype(float)
+    df['stop_lon'] = df['stop_lon'].astype(float)
+    df['zone_id'] = df['zone_id'].astype(int)
     data_types = ['INTEGER', 'TEXT', 'REAL', 'REAL', 'INTEGER']
     assert len(data_types) == len(columns_to_keep), 'Data type specification invalid.'
 
@@ -28,7 +33,7 @@ def automated_data_pipeline():
     df = df[df['zone_id'] == 2001]
 
     # validate that string field `stop_name` contains actually only alphabetic text
-    df = df[df['stop_name'].str.match(r'^[A-Za-z\säöüÄÖÜ\/-]+$')]  # set of charecters to allow
+    df = df[df['stop_name'].str.match(r'^[A-Za-z\säöüÄÖ,.ßÜ\/-]+$')]  # set of charecters to allow
 
     # validate coordinate value range
     min_val, max_val = -90.0, 90.0
@@ -41,9 +46,9 @@ def automated_data_pipeline():
     # persist data
     column_types = dict(zip(df.columns, data_types))
     conn = sqlite3.connect(DATABASE_PATH)
-    df.to_sql(TABLE_NAME, conn, if_exists='replace', index=False, dtype=column_types)
+    n = df.to_sql(TABLE_NAME, conn, if_exists='replace', index=False, dtype=column_types)
     conn.close()
-    print('Saved data into database.')
+    print(f'Saved {n} records into database at {os.path.abspath(DATABASE_PATH)}.')
 
     # clean artifacts
     shutil.rmtree('data')
